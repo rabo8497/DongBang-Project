37407e63d5ecda821d3442d764a9a4a692fee298
Pengdol2 <pengdol2@gm.gist.ac.kr>
2023-05-04 10:43:11 +0900

    add Book.h

--- book.h
+++ book.h
@@ -1,8 +1,24 @@
 #ifndef BOOK_H
 #define BOOK_H
 
-class Book {
+#include <string>
+#include <vector>
+#include "user.h"
 
+class Book
+{
+public:
+  Book(const int buid, std::string bookName, std::string bookSeries,std::string bookAuthor, std::string bookPub, const int bookDate, int bookCount);
+
+private:
+  const int buid;
+  std::string bookName;
+  std::string bookSeries;
+  int bookCount;
+  const int bookDate;
+  std::string bookAuthor;
+  std::string bookPub;
+  std::vector<std::string[3]> bookComm;
 };
 
 #endif
