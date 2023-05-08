49d46c60cb5f3c570f5a6c8a812d42f43180715c
ArsenicParadigm <62236125+zer0pacity@users.noreply.github.com>
2023-05-08 21:31:34 +0900

    Revert "Managers"

--- caelum.h
+++ caelum.h
@@ -1,18 +1,3 @@
-// Caelum : 공공재 클래스.
-// Account와 Device는 Caelum을 상속함.
-// getName() : 이름.
-// active() : 쓰이고 있으면 true를 반환함.
-// Update(State, User *) : 상태 전환용 "온"
-// 사용 예시 : Update(ACTIVE, (사용자 클래스 주소))
-// 이걸 쓰면 Usage 클래스에 정보가 기록됨
-// Update(State) : 상태 전환용 2 "오프"
-// 사용 예시 : Update(INACTIVE)
-// 주의 : 사용 중이 아니거나 Update(ACTIVE) 이러면 .Usage.user가 null pointer가 됨
-// Device 전용 기능 :
-// string game에 대한 getter/setter (현재 플레이 중인 게임)
-// Account 전용 기능 :
-// getType() : 계정의 유형(Leftel, Netfilx)을 반환함
-
 #ifndef CAELUM_H
 #define CAELUM_H
 
@@ -25,7 +10,6 @@ enum State
 #include <string>
 #include <ctime>
 #include "usage.h"
-#include "user.h"
 
 class Caelum
 {
@@ -33,16 +17,15 @@ private:
     std::string name;
     bool isActive;
     int id;
-    Usage &usage;
+    Usage usage;
 
 public:
     Caelum(int, std::string, bool);
     std::string getName();
-    int getId() const;
+    int getId();
     bool active();
-    void update(State, User *);
     void update(State);
-    bool operator<(const Caelum &right);
+    void update(State, Usage);
 };
 
 class Device : public Caelum
--- usage.h
+++ usage.h
@@ -1,11 +1,3 @@
-// Usage : 사용 정보 기록
-// 주의 : 사용 중이지 않을 때는 placeholder가 저장됨
-// 즉, 사용 중일 때만 참조 바람
-// getUser() : 사용자의 주소 값을 반환함
-// getTarget() : 사용 대상의 주소 값을 반환함
-// 주의 : Caelum* 형이어서 Account, Device 클래스의 기능을 쓰려면 포인터 캐스팅 필요
-// getStartTime() : 사용 시작 시간 반환
-
 #ifndef USAGE_H
 #define USAGE_H
 
@@ -22,7 +14,7 @@ private:
     std::time_t startTime;
 
 public:
-    Usage(User *user, Caelum *target);
+    Usage(std::string);
     User *getUser();
     Caelum *getTarget();
     std::time_t getStartTime();
