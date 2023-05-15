06625674a7a63d1970c4fd7461df3a19c9154e87
Pengdol2 <pengdol2@gm.gist.ac.kr>
2023-05-15 22:27:18 +0900

    0515

--- booktest.cpp
+++ booktest.cpp
@@ -12,24 +12,9 @@ int main(int argc, char *argv[])
 {
   BookManager BookManager;
   LendManager LendManager;
-  /*for (int i = 1; i <= 3; i++)
-  {
-    BookManager.bookadd("일상 " + to_string(i), "일상", "아라이 케이이치", "대원씨아이", "123456", 1, true);
-  }
-   for (int i = 1; i <= 3; i++)
-   {
-     BookManager.bookadd("Onii chan wa Oshimai " + to_string(i), "Onii chan wa Oshimai", "NekoTofu", "B Publisher", "456", 1, true);
-   }
-   for (int i = 1; i <= 3; i++)
-   {
-     BookManager.bookadd("ONE PUNCH MAN " + to_string(i), "ONE PUNCH MAN", "ONE", "C Publisher", "789", 1, true);
-   }
-   for (int i = 1; i <= 3; i++)
-   {
-     BookManager.bookadd("CHAINSAW MAN " + to_string(i), "CHAINSAW MAN", "Tatsuki", "D Publisher", "000", 1, true);
-   }*/
   UserManager usermanage;
-  BookManager.load(3);
+
+  BookManager.load(2);
 
   usermanage.signUp(20225167, "rabo84978", "aaaaa8", false, false);
   usermanage.signIn("rabo84987", "aaaaa8");
@@ -39,12 +24,29 @@ int main(int argc, char *argv[])
   LendManager.write(usermanage.getLoginedUser().getId(), BookManager.getBook().getBuid());
   usermanage.modifyFile(usermanage.getLoginedUser()); // 변한 User 객체를 저장하기 위해 파일 수정
 
+  BookManager.load(4);
+
+  BookManager.booklend(usermanage.getLoginedUser(), BookManager.getBook().getBuid());
+  LendManager.write(usermanage.getLoginedUser().getId(), BookManager.getBook().getBuid());
+  usermanage.modifyFile(usermanage.getLoginedUser());
+
   cout << usermanage.getLoginedUser().getLendBookNum() << " " << BookManager.getBook().getBCount() << " " << BookManager.getBook().getIsCanLend() << endl;
 
   BookManager.bookback(usermanage.getLoginedUser(), BookManager.getBook().getBuid());
   usermanage.modifyFile(usermanage.getLoginedUser());
 
   cout << usermanage.getLoginedUser().getLendBookNum() << " " << BookManager.getBook().getBCount() << " " << BookManager.getBook().getIsCanLend() << endl;
+
+  /*for (int i = 1; i <= 3; i++)
+  {
+    BookManager.bookadd("Physics " + to_string(i), "Physics", "Author A", "Publisher A", "20220101", 1, true);
+  }
+
+  for (int i = 1; i <= 3; i++)
+  {
+    BookManager.bookadd("Comics " + to_string(i), "Comics", "Author B", "Publisher B", "20230101", 1, true);
+  }*/
+
 
   return 0;
 }
--- lendManager.cpp
+++ lendManager.cpp
@@ -20,7 +20,7 @@ void LendManager::nowTime()
 {
   timer = time(NULL);
   t = localtime(&timer);
-  timeDate = std::to_string(t->tm_year + 1900) + (t->tm_mon < 9 ? "0" + std::to_string(t->tm_mon + 1) : std::to_string(t->tm_mon + 1)) + (t->tm_mday < 10 ? "0" + std::to_string(t->tm_mday) : std::to_string(t->tm_mon));
+  timeDate = std::to_string(t->tm_year + 1900) + (t->tm_mon < 9 ? "0" + std::to_string(t->tm_mon + 1) : std::to_string(t->tm_mon + 1)) + (t->tm_mday < 10 ? "0" + std::to_string(t->tm_mday) : std::to_string(t->tm_mday));
 }
 
 void LendManager::load(int) {}
