a422bd9fb4e04bfb8d96cfcb00f6276fa793c4ad
rabo8497 <jeon9799@gmail.com>
2023-05-28 23:16:00 +0900

    add comment

---
+++ books/Comment.h
@@ -0,0 +1,28 @@
+#ifndef COMMENT_H
+#define COMMENT_H
+
+#include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include "../FileHandler.h"
+#include "../users/User.h"
+#include "../books/Book.h"
+#include "../users/UserManager.h"
+
+#define RESET "\033[0m"
+#define RED "\033[31m"
+
+class Comment : public FileHandler {
+private:
+    int bookId;
+    std::vector<std::vector<std::string>> bookComments;
+public:
+    Comment(int);
+    void write(User, std::string);
+    void load();
+    void print_comments(UserManager&);
+};
+
+#endif
